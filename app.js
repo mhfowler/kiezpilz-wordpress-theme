0ce1ffe7406accac5db3878d477c13eafda7ab3f
mhfowler <maxhfowler@gmail.com>
2021-09-17 22:34:14 +0200

    Styling

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 
 $(document).ready(function() {
-    console.log('++ jquery is working heyyyyy')
+    console.log('++ jquery is working heyyyyy');
+
+    $('.select-german').click(function(e) {
+        e.preventDefault();
+        $('.language-option').removeClass('selected');
+        $(this).addClass('selected');
+        $(".english").hide();
+        $(".german").show();
+    });
+       $('.select-english').click(function(e) {
+        e.preventDefault();
+        $('.language-option').removeClass('selected');
+        $(this).addClass('selected');
+        $(".german").hide();
+        $(".english").show();
+    });
 });
+
+
+
--- index.php
+++ index.php
@@ -19,29 +19,54 @@ get_template_part('header');
         />
     </div>
 
-
-    <div class="writeup">
-Kiezpilz is a citizen-science collective unfolding the social and ecological benifits of communal fungi cultivation. We are making use of unused spaces and urban waste streams. Our goal is reproducable, community-based fungi cultivation.
-<br/><br/>
-We see fungi cultivation as a portal towards more sustainable ways of living, community empowerment, and reciprocal connection with non-human life.
-<br/><br/>
-Dreams
-<br/><br/>
-We dream of communities that enrich the natural world around them and enrich eachother in the process. Moving away from extraction and exploitation towards solidarity and autonomy. People empowered to define their own lives in cooperation with eachother and the non-human coconspiritors around them.
-<br/><br/>
-Fungi close the loop of natural cycles. They turn the fallen leaves into into rich living soil that supports the growth of a new tree. Fungi show us the connection of old and new, of death and rebirth.
-<br/><br/>
-We hope to foster community resilence by working with fungi - both as direct practice and as a source for reflection and metaphor.
-<br/><br/>
-Methods
-<br/><br/>
-Using low-tech and DIY methods, we cultivate mushrooms at a community scale. Our first space is a small oyster mushroom farm in the basement of Top Lab in Schillerkiez Neukolln. We grow the mushrooms using locally sourced waste materials as substrate, using sawdust from a local woodshop and coffee grounds from local coffee shops.
-<br/><br/>
-We encourage you to reproduce our experiment in your home or your neighborhood and are happy to share material and knowledge.
-<br/><br/>
-Telegram - Scuttlebutt - Email
+    <div class="language-select">
+        <div class="language-options">
+            <span class="select-english language-option selected">En</span> <span class="divider"> | </span> <span class="select-german language-option"> De </span>
+        </div>
     </div>
 
+
+    <div class="writeup">
+        <div class="english">
+            Kiez Pilz is a citizen-science collective unfolding the social and ecological benefits of communal fungi cultivation. We are making use of unused spaces and urban waste streams. Our goal is reproducible, community-based fungi cultivation.
+            <br/><br/>
+            We see fungi cultivation as a portal towards more sustainable ways of living, community empowerment, and reciprocal connection with non-human life.
+            <br/><br/>
+            <div class="centered">
+            Dreams
+            </div>
+            We dream of communities that enrich the natural world around them and enrich each other in the process. Moving away from extraction and exploitation towards solidarity and autonomy. People empowered to define their own lives in cooperation with each other and the non-human co-conspirators around them.
+            <br/><br/>
+            Fungi close the loop of natural cycles. They turn the fallen leaves into into rich living soil that supports the growth of a new tree. Fungi show us the connection of old and new, of death and rebirth.
+            <br/><br/>
+            We hope to foster community resilience by working with fungi &mdash; both as direct practice and as a source for reflection and metaphor.
+            <br/><br/>
+            <div class="centered">
+            Methods
+            </div>
+            Using low-tech and DIY methods, we cultivate mushrooms at a community scale. Our first space is a fruiting chamber in the basement of <a href="http://www.top-ev.de/">Top Lab</a> in Schillerkiez Neukölln where we grow Oyster Mushrooms, Lion's Mane and Reishi. We grow the mushrooms using locally sourced waste materials as substrate, using sawdust from a local wood-shop and coffee grounds from local coffee shops.
+            You can find more documentation of our process <a href="https://hyphalfusion.network/tag/kiezpilz">here</a>.
+            <br/><br/>
+            We encourage you to reproduce our experiment in your home or your neighborhood and are happy to share material and knowledge.
+            <br/><br/>
+                  <div class="centered" style="margin-top: 30px;">
+            Contact
+            </div>
+        </div>
+        <div class="german">
+            german version here
+            <br/><br/>
+        </div>
+
+      <div class="contact-links">
+        Email &mdash; <a href="mailto:contact@kiezpilz.de">contact@kiezpilz.de</a><br/>
+        Telegram &mdash;
+        <a href="https://t.me/joinchat/0JX49SM2jykxMjli">https://t.me/joinchat/0JX49SM2jykxMjli</a><br/>
+        <!-- Scuttlebutt &mdash;
+        <a href="https://ssb.kiezpilz.de">https://ssb.kiezpilz.de</a> -->
+    </div>
+
+    </div>
 </div>
 
 <?php
